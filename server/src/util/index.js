8e5f345c1bef8edfe2b722e7746d866334cf4b22
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-02 16:32:43 +0200

    add function to normalize data from entities

--- server/src/util/index.js
+++ server/src/util/index.js
@@ -12,7 +12,32 @@ function readPdfFile(filePath) {
     const fileBuffer = fs.readFileSync(normalizedPath);
     return pdf(fileBuffer);
 }
-
+function normalizeEntities(entities) {
+    const {entity_list,time_expression_list,money_expression_list} = entities ;
+    const res = [] ;
+    for(let entity of entity_list) {
+        const entityType = entity.sementity.type
+        if(entityType.includes('>')){
+            const value = entity.form ;
+            const subStringStart = entityType.indexOf('>')
+            const type = entityType.substring(subStringStart+1).split('>');
+            res.push({value,type}) ;
+        }
+    }
+    for(let time of time_expression_list) {
+        const value = time.form ;
+        const type = 'time';
+        const precision = time.precision ;
+        res.push({value,type,precision});
+    }
+    for(let money of money_expression_list) {
+        const value = money.form ;
+        const type = 'money';
+        const currency = money.currency ;
+        res.push({value,type,currency}) ;
+    }
+    return res;
+}
 module.exports = {
-    readPdfFile
+    readPdfFile,normalizeEntities
 }
