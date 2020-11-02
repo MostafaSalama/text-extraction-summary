1df6ffab632c2f4cd874f609e565aa424eaadeed
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-02 15:33:55 +0200

    use the service summarize funtion in the route and return the summary to the user

--- server/src/controllers/api.js
+++ server/src/controllers/api.js
@@ -1,20 +1,25 @@
 const uploadFile = require('../middleware/upload');
-const {readPdfFile} = require('../util')
+const { summarizeFile } = require('../service');
 async function uploadNewFile(request, response) {
 	try {
 		await uploadFile(request, response);
 
 		if (!request.file) {
-			return response.status(400).send({ message: 'Please upload the correct file format' });
+			return response
+				.status(400)
+				.send({ message: 'Please upload the correct file format' });
 		}
-		const {text} = await readPdfFile(request.file.path);
-		response.status(200).json({
-			text
-		})
-
+		const data = await summarizeFile(request.file.path);
+		if (!data.summary) {
+			response.json({
+				error: `error trying to summarize file ${file.originalname}`,
+				message: data.msg,
+			});
+		}
+		response.json({ summary: data.summary });
 	} catch (e) {
 		response.status(500);
-		response.json({error:e.message})
+		response.json({ error: e.message });
 	}
 }
 
