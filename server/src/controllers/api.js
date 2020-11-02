e911cb20c878435d067d7b20c63d1e235ef25e6b
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-02 13:04:13 +0200

    update api route to return the content of the pdf file with maxmum size 200kb

--- server/src/controllers/api.js
+++ server/src/controllers/api.js
@@ -1,4 +1,5 @@
 const uploadFile = require('../middleware/upload');
+const {readPdfFile} = require('../util')
 async function uploadNewFile(request, response) {
 	try {
 		await uploadFile(request, response);
@@ -6,12 +7,14 @@ async function uploadNewFile(request, response) {
 		if (!request.file) {
 			return response.status(400).send({ message: 'Please upload the correct file format' });
 		}
-		response.status(200).send({
-			message: 'Uploaded the file successfully: ' + request.file.originalname,
-		});
+		const {text} = await readPdfFile(request.file.path);
+		response.status(200).json({
+			text
+		})
+
 	} catch (e) {
 		response.status(500);
-		response.json({error:e})
+		response.json({error:e.message})
 	}
 }
 
