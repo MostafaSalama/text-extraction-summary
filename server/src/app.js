c0a3eec1bb062a49f0a39bafc6d3da6c384813dc
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-02 11:14:41 +0200

    update app.js to use all routes and middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,17 @@
 const dotEnv = require('dotenv');
 const express = require('express') ;
-
+const logger = require('./middleware/logger');
+const apiRoutes = require('./routes/api') ;
 dotEnv.config() ;
 
+// app setup
 const app = express();
-
 app.use(express.json()) ;
 app.use(express.urlencoded({extended:false})) ;
+
+// define middleware
+app.use(logger)
+// define routes
+app.use('/api',apiRoutes) ;
 
 module.exports = app ;
--- server/src/middleware/upload.js
+++ server/src/middleware/upload.js
@@ -1,20 +1,18 @@
-const util = require("util");
+const util = require('util');
 const path = require('path');
-const multer = require("multer");
+const multer = require('multer');
+const { resourcesPath } = require('../config');
 const maxSize = 2 * 1024 * 1024;
-let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null,  path.join(__dirname,'/resources/uploads'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    },
+const storage = multer.diskStorage({
+	destination: resourcesPath,
+	filename: function (req, file, cb) {
+		cb(null, `${Date.now()}-${file.originalname}`);
+	},
 });
-
 let uploadFile = multer({
-    storage: storage,
-    limits: { fileSize: maxSize },
-}).single("file");
+	storage: storage,
+	limits: { fileSize: maxSize },
+}).single('file');
 
 let uploadFileMiddleware = util.promisify(uploadFile);
 module.exports = uploadFileMiddleware;
